02af0fcf4577498d062c1700cc2482f0121b0b2d
drgarg <prabhat14@gmail.com>
2019-09-29 12:00:19 -0500

    added php

---
+++ form.php
@@ -0,0 +1,21 @@
+<?php
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+    $from = 'From: TangledDemo'; 
+    $to = 'prabhat.garg.md@gmail.com'; 
+    $subject = $_POST['subject'];
+    $human = $_POST['human'];
+			
+    $body = "From: $name\n E-Mail: $email\n Subject: $subject\n Message:\n $message";
+				
+    if ($_POST['submit'] && $human == '4') {				 
+        if (mail ($to, $subject, $body, $from)) { 
+	    echo '<p>Your message has been sent!</p>';
+	} else { 
+	    echo '<p>Something went wrong, go back and try again!</p>'; 
+	} 
+    } else if ($_POST['submit'] && $human != '4') {
+	echo '<p>You answered the anti-spam question incorrectly!</p>';
+    }
+?>
